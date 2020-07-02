4560f365c21b652c506c9f08896f0e48f25b4500
Preston Rollins <ptrollins@hotmail.com>
2020-07-02 15:05:24 -0400

    Refactored component file structure and added product and productoverview components; Added basic styling and page layout;

---
+++ client/src/components/product/Product.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ProductOverview from './overview/ProductOverview';
+
+const Product = () => (
+  <div className="content">
+    <section>
+      <ProductOverview />
+    </section>
+    <section></section>
+    <section></section>
+  </div>
+);
+
+export default Product;
