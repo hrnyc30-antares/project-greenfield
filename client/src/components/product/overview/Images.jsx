5df0b9411d908a01dd7b7e2c6a671a63d8796b3b
Preston Rollins <ptrollins@hotmail.com>
2020-07-05 23:43:59 -0400

    displays default image and thumbnails for product

--- client/src/components/product/overview/Images.jsx
+++ client/src/components/product/overview/Images.jsx
@@ -2,11 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const Images = () => <h2>Images</h2>;
+const Images = ({
+  dispatch,
+  loading,
+  photos,
+  currentPhoto,
+  product,
+  hasErrors,
+}) => {
+  if (loading) return <p>Loading product...</p>;
+  if (hasErrors) return <p>Unable to display product.</p>;
+
+  const renderThumbnails = () =>
+    photos.map((urls, i) => (
+      <div key={urls.url}>
+        <img src={urls.thumbnail_url} alt={`Product Thumbnail${i}`} />
+      </div>
+    ));
+
+  return (
+    <div className="product-media">
+      <div className="product-image-thumbnails">{renderThumbnails()}</div>
+      <div className="product-image">
+        <img src={currentPhoto.url} alt={product.name} />
+      </div>
+    </div>
+  );
+};
 
 Images.propTypes = {
   dispatch: PropTypes.func.isRequired,
   loading: PropTypes.bool.isRequired,
+  photos: PropTypes.arrayOf(PropTypes.object).isRequired,
+  currentPhoto: PropTypes.objectOf(PropTypes.string).isRequired,
   product: PropTypes.objectOf(
     PropTypes.oneOfType([PropTypes.array, PropTypes.string, PropTypes.number])
   ).isRequired,
@@ -14,12 +42,12 @@ Images.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  // key comes from reducers/index.js
-  // value comes from reducers/productReducer.js
   dispatch: PropTypes.func.isRequired,
-  loading: state.product.loading,
+  loading: state.styles.loading,
+  photos: state.styles.currentStyle.photos,
+  currentPhoto: state.styles.currentPhoto,
   product: state.product.product,
-  hasErrors: state.product.hasErrors,
+  hasErrors: state.styles.hasErrors,
 });
 
 export default connect(mapStateToProps)(Images);
