aac9a567ea040392fb2cd48211afe7e1a84a2f87
Jose Collado <jcoll1020@gmail.com>
2020-07-09 10:44:41 -0400

    added images in each tiles

--- client/src/components/common/tileList/Tile.jsx
+++ client/src/components/common/tileList/Tile.jsx
@@ -1,18 +1,17 @@
-
 import React from 'react';
 import HelpButton from './HelpButton';
-import ReportButton from './ReportButton'
+import ReportButton from './ReportButton';
 
 // think about using ...args as a parameter so that you can add more to it, will probably make seller sorting easier
-const Tile = ({widget, data }) => {
+const Tile = ({ widget, data }) => {
   const renderTile = () => {
     if (widget === 'qa') {
       return data.map((element) => (
-        <li key ={element.question_id}>
+        <li key={element.question_id}>
           <p>Question:</p>
           <span>{element.question_body}</span>
           <span>
-          <HelpButton widget="qa" data={element}/> | Add Answer
+            <HelpButton widget='qa' data={element} /> | Add Answer
           </span>
           <p>Answers:</p>
           <ul>
@@ -20,12 +19,34 @@ const Tile = ({widget, data }) => {
               .sort((a, b) => b.helpfulness - a.helpfulness)
               .map((value, idx) => {
                 if (idx < 2) {
-                  const dateFormat = new Date(value.date).toDateString().slice(4, value.date.length);
+                  const dateFormat = new Date(value.date)
+                    .toDateString()
+                    .slice(4, value.date.length);
                   return (
                     <li key={value.id}>
-                      <span>{value.body}, {dateFormat}</span>
+                      <span>{value.body}</span>
                       <span>
-                      <p> by {value.answerer_name}</p> | <HelpButton widget={'answer'} data={value} /> | <ReportButton widget={'answer'} data={value} />
+                        {value.photos.length > 1 ? (
+                          value.photos.map((el, index) => {
+                            return (
+                              <img
+                                src={el}
+                                alt='answer photo'
+                                key={index + Math.random() * 86}
+                                width='150px'
+                                height='150px'
+                              />
+                            );
+                          })
+                        ) : (
+                          <></>
+                        )}
+                        <p>
+                          {' '}
+                          by {value.answerer_name}, {dateFormat}
+                        </p>{' '}
+                        | <HelpButton widget={'answer'} data={value} /> |{' '}
+                        <ReportButton widget={'answer'} data={value} />
                       </span>
                     </li>
                   );
@@ -41,6 +62,5 @@ const Tile = ({widget, data }) => {
   };
   return <ul>{renderTile()}</ul>;
 };
-
 
 export default Tile;
