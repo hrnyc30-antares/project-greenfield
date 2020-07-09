5291e05c1ab288396a3935345c1d279cad9a56a3
Preston Rollins <ptrollins@hotmail.com>
2020-07-08 22:24:25 -0400

    Mobile swipe carousel; main image sizing; thumbnail alignment; Other css tweeks

--- client/src/components/product/overview/Images.jsx
+++ client/src/components/product/overview/Images.jsx
@@ -1,31 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { FullscreenIcon } from '@material-ui/icons';
 
-const Images = ({
-  dispatch,
-  loading,
-  currentStyle,
-  currentPhoto,
-  product,
-  hasErrors,
-}) => {
+const Images = ({ dispatch, loading, currentStyle, product, hasErrors }) => {
   if (loading) return <p>Loading product...</p>;
   if (hasErrors) return <p>Unable to display product.</p>;
 
+  // const [currentPhoto, setCurrentPhoto] = useState(currentStyle.photos[0]);
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+  const [leftPosition, setLeftPosition] = useState(0);
+  const [clientX, setClientX] = useState(0);
+  const [deltaX, setDeltaX] = useState(0);
+  const [thumbStartIndex, setThumbStartIndex] = useState(0);
+  const [thumbEndIndex, setThumbEndIndex] = useState(0);
+
   const renderThumbnails = () =>
     currentStyle.photos.map((urls, i) => (
-      <div key={urls.url}>
-        <img src={urls.thumbnail_url} alt={`Product Thumbnail${i}`} />
-      </div>
+      <button
+        type="button"
+        onClick={() => setCurrentPhotoIndex(i)}
+        key={urls.url}
+      >
+        <img
+          src={
+            urls.thumbnail_url ||
+            'https://edtmlv.pbworks.com/f/1450761659/media-6466-w700-q100%5B1%5D.jpg'
+          }
+          alt={`Product Thumbnail${i}`}
+        />
+      </button>
     ));
+
+  const renderDots = () =>
+    currentStyle.photos.map((urls, i) => (
+      <li
+        className={i === currentPhotoIndex ? 'active' : ''}
+        key={`bullet_${i}`}
+        onClick={() => setCurrentPhotoIndex(i)}
+      />
+    ));
+
+  const handleTouchStart = (e) => {
+    setClientX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e) => {
+    setDeltaX(e.changedTouches[0].clientX - clientX);
+    setLeftPosition(deltaX);
+  };
+
+  const handleTouchEnd = () => {
+    const { photos } = currentStyle;
+    if (deltaX < -100) {
+      if (currentPhotoIndex < photos.length - 1) {
+        setCurrentPhotoIndex(currentPhotoIndex + 1);
+      }
+    }
+    if (deltaX > 100) {
+      if (currentPhotoIndex > 0) {
+        setCurrentPhotoIndex(currentPhotoIndex - 1);
+      }
+    }
+    renderDots();
+    setLeftPosition(0);
+  };
 
   return (
     <div className="product-media">
       <div className="product-image-thumbnails">{renderThumbnails()}</div>
       <div className="product-image">
-        <img src={currentPhoto.url} alt={product.name} />
+        <img
+          src={
+            currentStyle.photos[currentPhotoIndex].url ||
+            'https://edtmlv.pbworks.com/f/1450761659/media-6466-w700-q100%5B1%5D.jpg'
+          }
+          alt={product.name}
+          onTouchStart={(touchStartEvent) => handleTouchStart(touchStartEvent)}
+          onTouchMove={(touchMoveEvent) => handleTouchMove(touchMoveEvent)}
+          onTouchEnd={() => handleTouchEnd()}
+          style={{ left: leftPosition }}
+        />
       </div>
+      <ol className="product-image-mobile">{renderDots()}</ol>
     </div>
   );
 };
@@ -41,7 +98,6 @@ Images.propTypes = {
       PropTypes.array,
     ])
   ).isRequired,
-  currentPhoto: PropTypes.objectOf(PropTypes.string).isRequired,
   product: PropTypes.objectOf(
     PropTypes.oneOfType([PropTypes.array, PropTypes.string, PropTypes.number])
   ).isRequired,
@@ -52,7 +108,6 @@ const mapStateToProps = (state) => ({
   dispatch: PropTypes.func.isRequired,
   loading: state.styles.loading,
   currentStyle: state.styles.currentStyle,
-  currentPhoto: state.styles.currentPhoto,
   product: state.product.product,
   hasErrors: state.styles.hasErrors,
 });
