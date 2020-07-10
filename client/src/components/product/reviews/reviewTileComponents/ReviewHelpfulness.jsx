edc32c5fd27617c73d974b910ef03767d76cbe51
AJ Benjumea <aj@AJs-Mac-mini.local>
2020-07-09 22:02:12 -0400

    Add reveiw helpfulness componenet with click functionality

---
+++ client/src/components/product/reviews/reviewTileComponents/ReviewHelpfulness.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const ReviewHelpfulness = ({helpfulness}) => {
+  const { id } = useParams();
+  const [helpfulCount, setHelpfulCount] = useState(helpfulness);
+  const [disable, setDisable] = useState('helpful-on');
+
+
+  const sendPutRequest = () => {
+    fetch(`http://18.224.200.47/reviews/helpful/${id}`, {
+      method: 'PUT',
+    })
+      .then(response => response.status) 
+      .then(result => {
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setHelpfulCount(helpfulCount + 1);
+    sendPutRequest();
+    if (disable === 'helpful-on') {
+      setDisable('helpful-off')
+    } else {
+      setDisable('helpful-on')
+    }
+  }
+
+  return (
+    <>
+      <div className={disable}>
+        {`Helpful ? Yes( `}
+          <a className={disable} value={1} onClick={handleClick} href="">{helpfulCount}</a>
+        {` )`}
+    </div>
+    </>
+  )
+}
+
+export default ReviewHelpfulness;
